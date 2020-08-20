6f8951e19dec076f085378257b09b9deb6e47f40
Nathan Burns <nathanandreburns@gmail.com>
2020-08-20 18:52:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -40,7 +40,9 @@ class Artist
     artist
   end
 
-  def print_songs 
-    self.songs.each {|song| song.name}
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
   end
 end
