237896a9b267415d20804f52cbd7cf62c196ea08
Nathan Burns <nathanandreburns@gmail.com>
2020-08-20 19:06:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,14 @@
 
 class Song
   attr_accessor :artist, :name
-  @@all = []
+  #@@all = []
 
   def initialize(name)
     @name = name
   end
   
   def self.all      
-    @@all
+    #@@all
   end
 
   def artist_name=(name)
