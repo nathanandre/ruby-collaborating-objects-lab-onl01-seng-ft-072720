10dd0a9c0807b6e6d9f5172d662d74a946ad8568
Nathan Burns <nathanandreburns@gmail.com>
2020-08-20 19:25:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,7 @@ class Song
 
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[0])
+    song = Song.new(song_info[1])
     song.artist_name = song_info[0]
     song
   end
