8adddab0ea0418ff9c4737362ffdceb8877cff17
Nathan Burns <nathanandreburns@gmail.com>
2020-08-20 18:56:24 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,4 +1,18 @@
-#class MP3Importer 
-  #def import(list_of_file_names)
-    #list_of_file_names.each { |filename| Song.new_by_file_name(filename) }
-#end 
+
+class MP3Importer
+
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+  end
+
+  def import
+    files.each {|song| Song.new_by_filename(song)}
+  end
+
+end
