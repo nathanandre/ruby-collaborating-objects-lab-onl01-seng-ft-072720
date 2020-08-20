754ccd38e4f11ac508452d07c99bdeb6861e17f7
Nathan Burns <nathanandreburns@gmail.com>
2020-08-20 18:31:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,7 +13,8 @@ class Artist
   end
 
   def add_song(song)
-    self.songs << song    
+    self.songs << song  
+    song.artist = self
   end
 
   def self.find_or_create_by_name(name)   #class method uses find/create class 
